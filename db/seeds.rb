286d34035b7c3f8192ea8220b51a980f2cec65ac
Trouni Tiet <trouni@gmail.com>
2023-11-19 22:37:40 +0900

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-Chatroom.create!(name: "general")
-Chatroom.create!(name: "tokyo")
-Chatroom.create!(name: "memes")
+Chatroom.find_or_create_by!(name: "general")
+Chatroom.find_or_create_by!(name: "tokyo")
+Chatroom.find_or_create_by!(name: "memes")
 
-User.create!(email: "trouni@me.com", username: 'trouni', nickname: "Trouni", password: "123123")
-User.create!(email: "doug@me.com", username: 'dmbf29', nickname: "Doug", password: "123123")
+trouni = User.find_or_initialize_by(email: "trouni@me.com")
+trouni.update!(username: 'trouni', nickname: "Trouni", password: "123123")
+doug = User.find_or_initialize_by(email: "doug@me.com")
+doug.update!(username: 'dmbf29', nickname: "Doug", password: "123123")
